1f34062ceddc78f601430e8c8a6cdf3426cc2393
Andrii Horbovskyi <117573529+g0rbochek@users.noreply.github.com>
2023-08-19 22:00:22 +0200

    haveDone

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const ulItems = document.querySelector('.gallery');
+console.log(ulItems);
+
+ulItems.insertAdjacentHTML('beforeend', creatMarkup(galleryItems));
+
+function creatMarkup(obj) {
+  return obj
+    .map(
+      ({ description, original, preview }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+    )
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+console.log(form);
+const LOCAL_KEY = 'feedback-form-state"';
+
+const user = JSON.parse(localStorage.getItem(LOCAL_KEY)) ?? {};
+console.log(user);
+localStorage.setItem(LOCAL_KEY, JSON.stringify(user));
+
+form.elements.email.value =
+  JSON.parse(localStorage.getItem(LOCAL_KEY)).email ?? '';
+
+form.elements.message.value =
+  JSON.parse(localStorage.getItem(LOCAL_KEY)).message ?? '';
+
+form.addEventListener('input', throttle(handlerMail, 500));
+
+form.addEventListener('submit', handlerSubmit);
+
+function handlerMail(e) {
+  user[e.target.name] = e.target.value;
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(user));
+  console.log(user);
+}
+
+function handlerSubmit(e) {
+  e.preventDefault();
+  console.log(user);
+  localStorage.removeItem(LOCAL_KEY);
+  form.reset();
+  user.message = '';
+  user.email = '';
+}
